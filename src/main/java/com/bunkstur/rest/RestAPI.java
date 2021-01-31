cb86c002ba05ef9ef0bde7310fe516ed6985fd15
Pratik Chowdhury <pratikc@live.co.uk>
2021-01-31 20:03:27 +0000

    [Backend] [Attendance] Add base REST API support

--- src/main/java/com/bunkstur/rest/RestAPI.java
+++ src/main/java/com/bunkstur/rest/RestAPI.java
@@ -5,14 +5,28 @@ import java.util.List;
 import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.SecurityContext;
 
+import com.bunkstur.Utils;
+import com.bunkstur.storage.attendance.Attendance;
+import com.bunkstur.storage.attendance.AttendanceAsyncService;
+import com.bunkstur.storage.attendance.AttendanceRest;
 import com.bunkstur.storage.subjects.SubjectAsyncService;
+import com.bunkstur.storage.users.User;
 import com.bunkstur.storage.users.UsersAsyncService;
 
+import io.reactivex.annotations.NonNull;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 
@@ -28,6 +42,9 @@ public class RestAPI {
 
     @Inject
     UsersAsyncService userAsyncService;
+
+    @Inject
+    AttendanceAsyncService attendanceAsyncService;
 
     @GET
     @Path("subjects")

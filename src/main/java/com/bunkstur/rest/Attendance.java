0f7527215cf445703711e42b6d1539fa2c649f78
Pratik Chowdhury <pratikc@live.co.uk>
2021-01-31 10:58:56 +0000

    Add Backend Subject table

--- src/main/java/com/bunkstur/rest/Attendance.java
+++ src/main/java/com/bunkstur/rest/Attendance.java
@@ -2,15 +2,21 @@ package com.bunkstur.rest;
 
 import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
+import com.bunkstur.storage.subjects.SubjectAsyncService;
+
 @Path("/api/v1/attendance")
 @RequestScoped
 @RolesAllowed("**")
 public class Attendance {
+    @Inject
+    SubjectAsyncService subjectAsyncService;
+
     @GET
     public String All() {
         return "All attend";
---
+++ src/main/java/com/bunkstur/storage/subjects/Columns.java
@@ -0,0 +1,5 @@
+package com.bunkstur.storage.subjects;
+
+public final class Columns {
+    public final static String SUBJECT_NAME = "name";
+}
